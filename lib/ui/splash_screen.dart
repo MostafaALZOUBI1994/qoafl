adcb09c9c2fca788d6e466dd480bea51cc8950ba
MostafaALZOUBI1994 <mostafaalzoubi1994@gmail.com>
2021-04-19 12:39:06 +0300

    before modify splashscreen

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -5,18 +5,37 @@ import 'package:qawafel/constants.dart';
 import 'package:qawafel/repository/userRepo.dart';
 import 'package:qawafel/ui/screens/main_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double variable=300;
+  @override
+  void initState() {
+    // TODO: implement initState
+    Future.delayed(Duration(milliseconds: 1000)).then((value) => {
+    move()
+    });
+
+    super.initState();
+  }
+  move(){
+    setState(() {
+      variable=0.0;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return CustomSplash(
-      imagePath: 'assets/logoluncher.png',
-      backGroundColor: backColor,
-      animationEffect: 'zoom-in',
-      logoSize: 200,
-      home: MainScreen(),
-      duration: 2500,
-      type: CustomSplashType.StaticDuration,
-
+    return Scaffold(
+      body: Stack(
+        children: [
+          AnimatedPositioned(top: variable,duration:Duration(milliseconds: 2500),
+            child: Container(height: 100,width: 100,child: Image.asset("assets/logoluncher.png"),)
+          ),
+        ],
+      ),
     );
   }
 }
